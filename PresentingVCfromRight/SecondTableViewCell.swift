fcf883649bb24312482c27e2a9f40e7526dbdb4e
Flamur <flamurbeqiri1@gmail.com>
2017-11-05 14:25:18 +0100

    initial commit

---
+++ PresentingVCfromRight/SecondTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  SecondTableViewCell.swift
+//  PresentingVCfromRight
+//
+//  Created by Flamur on 11/5/17.
+//  Copyright © 2017 Flamur. All rights reserved.
+//
+
+import UIKit
+
+class SecondTableViewCell: UITableViewCell {
+
+    @IBOutlet var labelTitle: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
